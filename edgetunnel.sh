39d243219d91300a84c8ee316aaa679120cd5149
cr4z3fr0g <1210564363@qq.com>
2024-06-10 08:57:16 +0000

    修改了edgetunnel.sh脚本,在退出终端后删除日志文件

--- edgetunnel.sh
+++ edgetunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+trap 'rm -rf /workspace/edgetunnel/nohup.out' EXIT
 export UUID=fdc387b0-c5d0-449d-a9f1-ac939bc44d00 PORT=4600
 nohup node /workspace/edgetunnel/dist/apps/node-vless/main.js &
